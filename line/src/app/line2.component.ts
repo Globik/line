ac1dc13d1fe48b3d1575577cccff19ac2e597195
Globik <globalikslivov@gmail.com>
2022-06-04 21:26:00 +0500

    spiral1

--- line/src/app/line2.component.ts
+++ line/src/app/line2.component.ts
@@ -2,6 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'line2-app',
-  template: `line2 app`,
+  template: './line2',
 })
 export class Line2Component {}
--- line/src/app/spiral1.component.ts
+++ line/src/app/spiral1.component.ts
@@ -1,7 +1,89 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  ViewContainerRef,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  ComponentRef,
+} from '@angular/core';
+import { FormGroup, FormControl, FormArray, NgModel } from '@angular/forms';
 
 @Component({
   selector: 'spiral1-app',
-  template: `spiral1 app`,
+  templateUrl: './spiral1.component.html',
 })
-export class Spiral1Component {}
+export class Spiral1Component implements AfterViewInit {
+  x: string = '';
+  y: string = '';
+  @ViewChild('canvasEl2', { static: false })
+  canvas: ElementRef<HTMLCanvasElement>;
+
+  private ctx: CanvasRenderingContext2D | null;
+  randomPoints: any = [];
+  ngAfterViewInit(): void {
+    this.ctx = this.canvas.nativeElement.getContext('2d');
+    if (this.ctx) {
+      for (let i = 0; i < 11; i++) {
+        this.randomPoints.push({
+          x: Math.floor(Math.random() * 300),
+          y: Math.floor(Math.random() * 300),
+        });
+      }
+      this.drawPoints();
+    }
+  }
+  drawPoints() {
+    if (this.ctx)
+      this.ctx.clearRect(
+        0,
+        0,
+        this.canvas.nativeElement.width,
+        this.canvas.nativeElement.height
+      );
+    this.randomPoints.forEach((el: any, i: number) => {
+      if (this.ctx) {
+        this.ctx.fillStyle = 'red';
+        this.ctx.fillRect(el.x, el.y, 4, 4);
+      }
+    });
+  }
+  drawSpiral() {
+    let gap = 7.8;
+    let width = this.canvas.nativeElement.width,
+      height = this.canvas.nativeElement.height; //размеры канвы
+    let cx = Math.floor(width / 2),
+      cy = Math.floor(height / 2); //центр канвы
+    if (this.ctx) {
+      this.ctx.clearRect(0, 0, width, height);
+      this.ctx.moveTo(cx, cy);
+      const STEPS_PER_ROTATION = 60; //шагов на круг
+      let increment = (2 * Math.PI) / STEPS_PER_ROTATION;
+      let theta = increment;
+      let ROTATIONS = this.randomPoints.length; //количество вращений
+      while (theta < ROTATIONS) {
+        let newX = cx + theta * Math.cos(theta) * gap;
+        let newY = cy - theta * Math.sin(theta) * gap;
+        this.ctx.lineTo(newX, newY);
+        this.ctx.fillStyle = 'red';
+        this.ctx.fillRect(newX, newY, 10, 10);
+        theta += increment * this.randomPoints.length;
+      }
+      this.ctx.stroke();
+    }
+  }
+
+  addPoint(x: NgModel, y: NgModel) {
+    if (!x || !y) {
+      alert('No x or y data provided!');
+      return;
+    }
+    this.randomPoints.push({ x: Number(x), y: Number(y) });
+    if (this.ctx) {
+      this.ctx.fillStyle = 'blue';
+      this.ctx.fillRect(Number(x), Number(y), 4, 4);
+    }
+
+    this.x = '';
+    this.y = '';
+  }
+}
